30f1cb1f1ec71a97803ef6e75902bd3fc9d6c565
“holazel” <dazeltodd@gmail.com>
2017-09-03 02:05:44 -0400

    5.1 routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,15 @@
 class StaticPagesController < ApplicationController
+	def about
+		redirect_to static_pages_landing_page_url
+	end
+
+	def contact
+	end
+
 	def index
 	end
-  def landing_page    
-    @products = Product.limit(2)
 
-  end
+  	def landing_page    
+    	@products = Product.limit(2)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   get 'static_pages/contact'
+  get 'static_pages/landing_page'
 
   get 'static_pages/index'
-  root 'static_pages#landing_page'
+  get 'static_pages/random'
+  root 'static_pages#contact'
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
