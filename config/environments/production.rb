8570504b23c12d72b47899001c2dd0c7506e04e9
“holazel” <dazeltodd@gmail.com>
2017-11-04 02:23:35 -0400

    6.13

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://mybikeshop.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://mybikeshop.herokuapp.com', 'http://mybikeshop.herokuapp.com']
+
 end
